33e9af4ff874acd52dd84b35d3b226793c6ab71b
Leon Zamel <leon.zamel@gmail.com>
2022-05-07 10:40:52 +0200

    Implement hittable sphere

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use std::fs;
 use std::ops;
 
-const IMAGE_WIDTH: usize = 256;
-const IMAGE_HEIGHT: usize = 256;
-
 #[derive(Copy, Clone)]
 struct Vec3 {
     x: f64,
@@ -43,6 +40,16 @@ impl ops::Add<Vec3> for Vec3 {
         }
     }
 }
+impl ops::Sub<Vec3> for Vec3 {
+    type Output = Self;
+    fn sub(self, _rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x - _rhs.x,
+            y: self.y - _rhs.y,
+            z: self.z - _rhs.z,
+        }
+    }
+}
 impl ops::Mul<f64> for Vec3 {
     type Output = Self;
     fn mul(self, _rhs: f64) -> Vec3 {
@@ -63,6 +70,16 @@ impl ops::Div<f64> for Vec3 {
         }
     }
 }
+impl ops::Neg for Vec3 {
+    type Output = Self;
+    fn neg(self) -> Vec3 {
+        Vec3 {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
 
 #[derive(Copy, Clone)]
 struct Ray {
@@ -76,7 +93,83 @@ impl Ray {
     }
 }
 
+struct Hit {
+    p: Vec3,
+    normal: Vec3, // Always points opposite to hit ray
+    t: f64,
+    front_face: bool, // If the face that was hit was the front, i.e. outward face
+}
+impl Hit {
+    fn new(p: Vec3, outward_normal: Vec3, t: f64, ray: &Ray) -> Hit {
+        let (front_face, normal) = Hit::to_face_normal(ray, outward_normal);
+        Hit {
+            p,
+            normal,
+            t,
+            front_face,
+        }
+    }
+    fn to_face_normal(ray: &Ray, outward_normal: Vec3) -> (bool, Vec3) {
+        let front_face = ray.direction.dot(&outward_normal) < 0.0;
+        let normal = {
+            if front_face {
+                outward_normal
+            } else {
+                -outward_normal
+            }
+        };
+        (front_face, normal)
+    }
+}
+
+trait Hittable {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<Hit>;
+}
+
+struct Sphere {
+    center: Vec3,
+    radius: f64,
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<Hit> {
+        let oc = ray.origin.clone() - self.center.clone();
+        let a = ray.direction.length_squared();
+        let half_b = oc.dot(&ray.direction);
+        let c = oc.length_squared() - self.radius * self.radius;
+        let discriminant = half_b * half_b - a * c;
+        if discriminant < 0.0 {
+            return None;
+        }
+        let sqrtd = discriminant.sqrt();
+
+        let root = (-half_b - sqrtd) / a;
+
+        if root < t_min || t_max < root {
+            let root = (-half_b + sqrtd) / a;
+            if root < t_min || t_max < root {
+                return None;
+            }
+        }
+        let t = root;
+        let p = ray.at(root);
+        let outward_normal = (p - self.center) / self.radius;
+        Some(Hit::new(p, outward_normal, t, ray))
+    }
+}
+
 fn main() {
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: usize = 400;
+    const IMAGE_HEIGHT: usize = 225; // == IMAGE_WIDTH / ASPECT_RATIO
+
+    let viewport_width: f64 = 2.0;
+    let viewport_height: f64 = viewport_width / ASPECT_RATIO;
+    let focal_length: f64 = 1.0;
+
+    let origin = Vec3::new(0.0, 0.0, 0.0);
+
+    // Init
     let mut image: Vec<Vec<Vec3>> = vec![
         vec![
             Vec3 {
@@ -92,13 +185,21 @@ fn main() {
     for j in 0..IMAGE_HEIGHT {
         for i in 0..IMAGE_WIDTH {
             image[j][i] = Vec3 {
-                x: 1.0 * j as f64,
-                y: 1.0 * i as f64,
-                z: 0.5 * 255.0,
+                x: j as f64 / IMAGE_HEIGHT as f64,
+                y: j as f64 / IMAGE_HEIGHT as f64,
+                z: 1.0 * 255.0,
             }
         }
     }
 
+    // World
+    let mut objects: Vec<Box<dyn Hittable>> = Vec::new();
+    objects.push(Box::new(Sphere {
+        center: Vec3::new(0.0, 0.0, -1.0),
+        radius: 0.5,
+    }));
+
+    // Write to file
     let mut data = "P3\n".to_string()
         + " "
         + &IMAGE_WIDTH.to_string()
